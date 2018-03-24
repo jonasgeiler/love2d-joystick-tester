a948c74476d6895b5ce2daeed9ff9489ab6857c1
Skayo <jonasg.cool@gmail.com>
2018-03-25 00:22:09 +0100

    Adding Joysticks while application is running

--- main.lua
+++ main.lua
@@ -1,32 +1,41 @@
 local joysticks = {}
-local currentJoystick = 0
+local currentJoystick = 1
 
-function love.load()
-	for i, joystick in ipairs(love.joystick.getJoysticks()) do
-		newJoystick = {}
-		newJoystick.name = joystick:getName()
-		newJoystick.axesNum = joystick:getAxisCount()
-		newJoystick.buttonsNum = joystick:getButtonCount()
-		newJoystick.hatsNum = joystick:getHatCount()
-		newJoystick.guid = joystick:getGUID()
-		newJoystick.id = joystick:getID()
-		if joystick:isVibrationSupported() then newJoystick.hasVibration = "Yes" else newJoystick.hasVibration = "No" end
-		if joystick:isGamepad() then newJoystick.isGamepad = "Yes" else newJoystick.isGamepad = "No" end
-		newJoystick.obj = joystick
-		
-		joysticks[#joysticks+1] = newJoystick
-	end
+function love.joystickadded(joystick)
+    newJoystick = {}
+	newJoystick.name = joystick:getName()
+	newJoystick.axesNum = joystick:getAxisCount()
+	newJoystick.buttonsNum = joystick:getButtonCount()
+	newJoystick.hatsNum = joystick:getHatCount()
+	newJoystick.guid = joystick:getGUID()
+	newJoystick.id = joystick:getID()
+	if joystick:isVibrationSupported() then newJoystick.hasVibration = "Yes" else newJoystick.hasVibration = "No" end
+	if joystick:isGamepad() then newJoystick.isGamepad = "Yes" else newJoystick.isGamepad = "No" end
+	newJoystick.obj = joystick
 	
-	if #joysticks > 0 then
-		currentJoystick = 1
-	end
+	joysticks[newJoystick.id] = newJoystick
 end
 
 function love.draw()
 	text = "Press space to go to next Joystick!\nPress enter to let the controller vibrate!\n\n"
 	
+	if love.joystick.getJoystickCount() == 0 then
+		text = text .. "No Joysticks connected."
+		love.graphics.print(text, 10, 10)
+		return
+	end
+	
+	text = text .. "Joysticks connected: " .. love.joystick.getJoystickCount() .. "\n\n"
+	
 	joystick = joysticks[currentJoystick]
 	text = text .. "Joystick " .. currentJoystick .. " - " .. joystick.name .. ":\n"
+	
+	if not joystick.obj:isConnected() then
+		text = text .. "    Joystick not connected."
+		love.graphics.print(text, 10, 10)
+		return
+	end
+	
 	text = text .. "    GUID: " .. joystick.guid .. "\n"
 	text = text .. "    ID: " .. joystick.id .. "\n"
 	text = text .. "    Number of Axes: " .. joystick.axesNum .. "\n"
@@ -78,11 +87,11 @@ end
 function love.keypressed(key)
 	if key == "space" then
 		currentJoystick = currentJoystick + 1
-		if currentJoystick > love.joystick.getJoystickCount() then
+		if currentJoystick > #joysticks then
 			currentJoystick = 1
 		end
 	elseif key == "return" then
-		if joysticks[currentJoystick].obj:isVibrationSupported() then
+		if love.joystick.getJoystickCount() > 0 and joysticks[currentJoystick].obj:isVibrationSupported() then
 			for i=0, 1, 0.1 do
 				joysticks[currentJoystick].obj:setVibration(i, i, i/2)
 				love.timer.sleep(i / 2)
